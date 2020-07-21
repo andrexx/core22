65ce9ad04ae2eabd252feabed1212dc9a34ec55f
andrii.chumak <andrexx@gmail.com>
2020-07-21 23:29:08 +0300

    Add project files.

---
+++ Core22.Sample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Core22.Sample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
